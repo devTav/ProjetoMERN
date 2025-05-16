5a7e41afe55fa6a904932fac70ad8c11176d1f54
devTav <gustavoluizsantosfaria@gmail.com>
2025-05-16 14:12:31 -0300

    🟢 add: cria pasta models com schema de produto (product.model.js)

---
+++ Backend/models/product.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+   name:{
+    type: String,
+    required: true
+   },
+   price:{
+    type: Number,
+    required: true
+   },
+   image:{
+    type: String,
+    required: true
+   } 
+
+
+}, 
+
+{timestamps: true // createdAt, updatedAt
+    }
+
+);
+
+const Product = mongoose.model('Product', productSchema);
+//products <- mongoDB
+
+export default Product;
